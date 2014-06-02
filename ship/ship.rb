881e9195d564e712cc5c63e695cd182f3e67b4f6
GeoffGeis <geoffgeis@hotmail.com>
2014-06-02 02:59:53 -0500

    multiships

--- ship/ship.rb
+++ ship/ship.rb
@@ -5,6 +5,6 @@ class Ship
     @row = Random.rand(0..b.board.length - 1)
     @col = Random.rand(0..b.board[0].length - 1)
     @orientation = ["up", "side"].sample
-    @size = Random.rand(1..5 - 1)
+    @size = Random.rand(1..5)
   end
 end
