3b557a4973507aa17b52eaaba883eab6df8af446
GeoffGeis <geoffgeis@hotmail.com>
2014-06-02 00:15:26 -0500

    made ship object board instance more readable

--- ship/ship.rb
+++ ship/ship.rb
@@ -1,10 +1,10 @@
 class Ship
   attr_accessor :orientation, :size, :row, :col
 
-  def initialize(board)
+  def initialize(b)
     @orientation = ["up", "side"].sample
     @size = Random.rand(1..5 - 1)
-    @row = Random.rand(0..board.board.length - 1)
-    @col = Random.rand(0..board.board[0].length - 1)
+    @row = Random.rand(0..b.board.length - 1)
+    @col = Random.rand(0..b.board[0].length - 1)
   end
 end
