1b272214446c98790391b4ee0f845f6e2736ecef
GeoffGeis <geoffgeis@hotmail.com>
2014-06-02 01:30:22 -0500

    testing objects

--- ship/ship.rb
+++ ship/ship.rb
@@ -1,10 +1,10 @@
 class Ship
-  attr_accessor :orientation, :size, :row, :col
+  attr_accessor :row, :col, :orientation, :size
 
   def initialize(b)
-    @orientation = ["up", "side"].sample
-    @size = Random.rand(1..5 - 1)
     @row = Random.rand(0..b.board.length - 1)
     @col = Random.rand(0..b.board[0].length - 1)
+    @orientation = ["up", "side"].sample
+    @size = Random.rand(1..5 - 1)
   end
 end
