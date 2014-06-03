10e48ce59d189ddb45018e3e06acaac437184fcf
GeoffGeis <geoffgeis@hotmail.com>
2014-06-02 20:51:38 -0500

    created proxi board for comparing hits to actual board.

--- battleship_2.rb
+++ battleship_2.rb
@@ -2,10 +2,11 @@ require './board/board.rb'
 require './ship/ship.rb'
 
 class Battleship
-  attr_accessor :board, :ship_count, :ships
+  attr_accessor :board, :proxi, :ship_count, :ships
 
   def initialize 
     @board = Board.new
+    @proxi = Board.new
     @ship_count = gets.chomp.to_i
     @ships = (1..@ship_count).map { |i| i = Ship.new(@board) }
   end
@@ -13,9 +14,9 @@ class Battleship
   def puts_ships
     @ships.each do |i|
       if i.orientation == "up"
-        (1..i.size).each { |x| @board.board[i.row - x][i.col - 1] = "X" }
+        (1..i.size).each { |x| @proxi.board[i.row - x][i.col - 1] = "X" }
       else
-        (1..i.size).each { |x| @board.board[i.row - 1][i.col - x] = "X" }    
+        (1..i.size).each { |x| @proxi.board[i.row - 1][i.col - x] = "X" }    
       end
     end
   end
